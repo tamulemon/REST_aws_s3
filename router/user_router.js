a05b513cf37e4b26f2c34b9d0aed9b7eb66fba3e
Meng Chen <tamulemon@gmail.com>
2015-07-26 14:41:05 -0700

    implement all the routes

--- router/user_router.js
+++ router/user_router.js
@@ -4,7 +4,7 @@
 //
 //GET /users/:user - done
 //
-//PUT /users/:user (rename a user and user's bucket) - done
+//PUT /users/:user (rename a user and user's folder) - done
 //
 //DELETE /users/:user - done
 //
@@ -14,22 +14,21 @@
 //
 //GET /user/:user/files/:file
 //
-//PUT /user/:user/files/:file (replace an already existing file, or update it somehow. 
+//PUT /user/:user/files/:file (replace an already existing file, or update it somehow. - done
 //
-//DELETE /user/:user/files (deletes all files. 
+//DELETE /user/:user/files (deletes all files. - done
 
 // {"fileName": "fileOne": "content": "hello world!"}
 
 // binary data
 
-// http://s3-aws-region.amazonaws.com/bucket
 var express = require('express');
 var bodyParser = require('body-parser');
 var User = require('../model/user.js');
 var File = require('../model/file.js');
 var AWS = require('aws-sdk');
 
-// because everyone is going to share the same bucket, this is hardcoded here
+// because the requirement is updated to all users are going to share the same bucket, it is hardcoded here or can be passed through environment variable
 var bucketName = process.env.Bucket || '55b13a28b651c557054eb832';
 
 var s3 = new AWS.S3();
@@ -38,10 +37,10 @@ var awsRegion = 'us-east-1';
 
 module.exports = function(router) {
 	router.use(bodyParser.json());
-	
+
 	router.route('/users')
 	.get(function(req, res) {
-		// display all users
+		// GET /users. display all users
 		User.find({}, function(err, users) {
 			if (err) {
 				res.json(errorHandler(err)('load users'));
@@ -52,7 +51,8 @@ module.exports = function(router) {
 		})
 	})
 	.post(function(req, res) {
-		// create a new user. a folder for this user is not created at this point. a bucket is also not created because users are sharing the same bucket
+		// POST /users
+		//create a new user. a folder for this user is not created at this point. a bucket is also not created because users are sharing the same bucket
 		var user = new User(req.body); 	
 		user.save(function(err, userData) {
 			if(err) { 
@@ -65,13 +65,15 @@ module.exports = function(router) {
 	});
 	
 	router.route('/users/:user/') 
-	// get all information about a user
+	// GET /users/:user
+	//get all information about a user
 	.get(function(req, res) {
 		fetchUser(req, res, function(err, user) {
 			res.json(user);
 		});
 	})
 	.put(function(req, res) {	
+	// PUT /users/:user
 	// update a user's name and update his online folder name
 		var oldUserName = req.params.user,
 				newUserName = req.body.name;	
@@ -90,7 +92,7 @@ module.exports = function(router) {
 						var oldParams = new setCopyParams(bucketName, file);
 						var fileName = file.Key.split('/').pop();
 						var newParams = new setParams(bucketName, newUserName + '/' + fileName);
-						var deleteParams = new setDelteParams(bucketName, file);
+						var deleteParams = new setDeleteParams(bucketName, file);
 						copy_put_delete_AwsFile(oldParams, newParams, deleteParams, function(er, data) {
 							if (er) {
 								res.json(errorHandler(err)('update user name'));
@@ -121,72 +123,36 @@ module.exports = function(router) {
 			})
 		})
 	})
-//		User.update({name: oldUserName}, {$set:{name: newUserName}}, function(err, user) {
-//			if (err) {
-//				res.json(errorHandler(err)('update user'));
-//			} 
-//		
-//			else {
-//				returnAwsFilesByUser(oldUserName, function(error, data) {
-//					if(error) {
-//						res.json(errorHandler(err)('update user name'));
-//					} 
-//					else{ 
-//						var fileCompleted = 0; 
-//						// have to implement a total count to dictate when to send response message. because there is a loop here, if the res.json is put within the last call back , the first file completed will trigger res.json, and the following will have an error: couldn't set header when response is send
-//						var total = data.length; // need to cash data.length here because there is another 'data' variable in callback scope
-//						data.forEach(function(file) {
-//							var oldParams = new setCopyParams(bucketName, file);
-//							var newParams = new setParams(bucketName, newUserName + '/' + file.Key.split('/').pop());
-//							var deleteParams = new setDelteParams(bucketName, file);
-//							copy_put_delete_AwsFile(oldParams, newParams, deleteParams, function(er, data) {
-//								if (er) {
-//									res.json(errorHandler(err)('update user name'));
-//								} else {
-//									fileCompleted ++;
-////									var urlParams = new setUrlParams(bucketName, user.name + '/' + req.body.fileName);
-////									var AWSurl = s3.getSignedUrl('getObject', urlParams);
-////									var newFile = new File({name: req.body.fileName, url: AWSurl});
-////									newFile.save(function(error, data) {
-//									if (fileCompleted === total) res.json({msg:'user name and file update completed'});	
-//								}
-//							});
-//						});
-//					}
-//				})
-//			}
-//		})
-//	})
 	.delete(function(req, res) {
+	// DELETE /users/:user
 	// delete a user and his folder and all his files
-		User.remove({name: req.params.user}, function(err, user) {
-			if (err) {
-				res.json(errorHandler(err)('delete user'));
-			}
-			else {
-				returnAwsFilesByUser(req.params.user, function(err, data) {
-					if(err) {
-						res.json(errorHandler(err)('delete user'));
-					} else {
-						var fileCompleted = 0;
-						var total = data.length;
-						data.forEach(function(file) {
-						var deleteParams = new setDelteParams(bucketName, file);
-							s3.deleteObject(deleteParams, function(err, data) {
-								if(err) return console.log('couldn\'t delete the file', err);
-								console.log('user folder is deleted from aws');
-								fileCompleted ++;
-								if (fileCompleted === total) res.json({msg: 'user/user folder/files are all deleted'});
-							})
-						})
-					}
-				})
-			}
+		fetchUser(req, res, function(err, user) {
+			var fileIds = [];
+			user.file.forEach(function(file) {
+				fileIds.push(file._id);
+			})
+			File.remove({ _id: { $in: fileIds } }, function (err) {
+				if (err) {
+					res.json(errorHandler(err)('delete files of user'));
+				}	else {
+					deleteAllUserFiles(user.name, function(err, data) {
+						if (err) {
+							console.log(err);
+							res.json(errorHandler(err)('delete user\'s files'));
+						} else {
+							user.remove();
+							res.json({msg: 'uer and all files deleted'});
+						}
+					});
+				}
+			})
 		})
 	})
 
 	
 	router.route('/users/:user/files')
+	//GET /user/:user/files 
+	// display all files and their urls for a user
 	.get(function(req, res) {
 		fetchUser(req, res, function(err, user) {
 			var fileOutput = {};
@@ -197,7 +163,15 @@ module.exports = function(router) {
 		})
 	})
 	.post(function(req, res) {
+		//POST /user/:user/files
+		// post a file, pass through req.body
 		fetchUser(req, res, function(err, user) {
+			for (var i = 0; i < user.file.length; i ++) {
+				// unique file name per user
+				if (user.file[i].name === req.body.fileName) { 
+					res.json({msg: 'file name already exists for this user'});
+				}
+			}
 			var params = new setPutParams(bucketName, user.name + '/' + req.body.fileName, req.body.content);
 			s3.putObject(params, function(error, data) {
 			 if (error) {
@@ -223,30 +197,123 @@ module.exports = function(router) {
 		})
 	})
 	.delete(function(req, res) {
-		User.update({name: req.params.user}, {$set:{file: []}}, function(err, user) {
-			if (err) {
-				res.json(errorHandler(err)('delete files of user'));
-			} 
-			else if (!user) {
-				res.send({msg:'couldn\'t find user'});
-			}
-			else {
-				console.log('more');
-			}
-	})
+		//DELETE /user/:user/files 
+		// delete all files under a user, but the user will be kept
+		fetchUser(req, res, function(err, user) {
+			var fileIds = [];
+			user.file.forEach(function(file) {
+				fileIds.push(file._id);
+			})
+//			console.log(user);
+//			console.log(fileIds);
+			user.file = [];
+			user.save();
+			File.remove({ _id: { $in: fileIds } }, function (err) {
+				if (err) {
+					res.json(errorHandler(err)('delete files of user'));
+				}	else {
+					deleteAllUserFiles(user.name, function(err, data) {
+						if (err) {
+							console.log(err);
+							res.json(errorHandler(err)('delete user\'s files'));
+						} else {
+							res.json({msg: 'all files deleted'});
+						}
+					});
+				}
+			})
+		})
 	})
 
-//	router.route('/users/:user/files/:file')
-//	.get(function(req, res) {
-//
-//			});
-//	
-//	})
-//	.put()
-//	.delete()
+
+	router.route('/users/:user/files/:file')
+	//GET /user/:user/files/:file
+	// view a specific file and its url
+	.get(function(req, res) {
+		fetchUser(req, res, function(err, user) {
+			user.file.forEach(function(file) {
+				if (file.name === req.params.file) {
+					res.json({name: file.name, url: file.url});
+				}
+			})	
+		});
+	})
+	.put(function(req, res) {
+		// PUT /user/:user/files/:file
+		// update the content of a file.  
+		fetchUser(req, res, function(err, user) {
+			var fileExist;
+			user.file.forEach(function(file) {
+				if (file.name === req.params.file) {
+					fileExist = true;
+					var newParams = new setPutParams(bucketName, user.name + '/' + req.params.file, req.body.content);
+					var urlParams = new setUrlParams(bucketName, user.name + '/' + req.params.file);
+					var deleteParams = {Bucket: bucketName, Key: user.name + '/' + req.params.file};
+					update_AwsFile_returnUrl(newParams, deleteParams, urlParams, function(err, newUrl) {
+						if (err) {
+							res.json(errorHandler(err)('update user\'s file'));
+							
+						} else {
+							// this is not needed actually. If the bucket and key stays the same, AWS s3 will reuse the signedURL so the url should stay the same!
+							File.update({_id: user.file._d}, {$set: {url: newUrl}}, function(err) {
+								if(err) {
+									res.json(errorHandler(err)('update file url'));
+								}
+								else {
+									res.json({msg: 'file updated'});
+								}
+							});
+						}
+					})
+				}
+			})
+			if(!fileExist) {
+					res.json({msg: 'the file doesn\'t exist'});
+				}
+		})
+	})
+	.delete(function(req, res) {
+		// DELETE /user/:user/files/:file
+		// delete a specific file
+		fetchUser(req, res, function(err, user) {
+			var fileExist;
+			for (var i = 0; i < user.file.length; i++) {
+				console.log(i);
+				var file = user.file[i];
+				if (file.name === req.params.file) {
+					fileExist = true;
+					var deleteParams = {Bucket: bucketName, Key: user.name + '/' + req.params.file};
+					s3.deleteObject(deleteParams, function(err, data) {
+						if (err) {
+							res.json(errorHandler(err)('delete user\'s file'));
+						} else {
+							File.remove({_id: file._id}, function(err) {
+								if(err) {
+									res.json(errorHandler(err)('delete file from database'));
+								}
+								else {
+									user.file.splice(i, 1);
+									user.save();
+									res.json({msg: 'file deleted'});
+								}
+							});
+						}
+					})
+					break; 
+				}
+			}
+			if(!fileExist) {
+					res.json({msg: 'the file doesn\'t exist'});
+				}
+		})
+	})
+
+
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 //
+// helper functions
 					
 
 function errorHandler(error) {
@@ -272,7 +339,7 @@ function setPutParams(bucketName, key, body) {
 	this.Body = body;
 }
 
-function setDelteParams(bucket, file) {
+function setDeleteParams(bucket, file) {
 	this.Bucket = bucket;
 	this.Key = file.Key;
 }
@@ -305,29 +372,6 @@ function fetchUser(req, res, callback) {
 		}
 	})
 }
-
-//function fetchUser(userName, callbak) {
-//	User.findOne({name: userName}, callback(error, user) {
-//		if (err) {
-//			callback(error);
-//		}
-//		else {
-//			user.populate('file');
-//			callback(null, user);
-//		}
-//	});
-//}
-
-//function createAwsFile(params, res) {
-//	s3.putObject(params, function(err, data) {
-//	 if (error) {
-//		 console.log(err);
-//		 res.status(500);
-//		 res.json({msg: 'Couldn\'t save file'});
-//	 }
-//	 res.json({msg: 'File is saved'});
-//	})
-//};
 
 function returnAwsFilesByUser(userName, callback) {
 	var params = new setListParams(bucketName, userName);
@@ -369,12 +413,49 @@ function copy_put_delete_AwsFile(oldParams, newParams, deleteParams, callback) {
 		}
 	})
 }
-		
-											
+
+// this is not needed actually. If the bucket and key stays the same, AWS s3 will reuse the signedURL so the url should stay the same!
+// I am just keeping this to be extra save, in case AWS change their workflow in the future
+function update_AwsFile_returnUrl(newParams, deleteParams, urlParams, callback) {
+	s3.deleteObject(deleteParams, function(err, data) {
+		if(err) {
+			console.log('couldn\'t delete the old file');
+			callback(err);
+		} else {
+			s3.putObject(newParams, function(err, data) {
+				if(err) {
+					console.log('couldn\'t create new file');
+					callback(err);
+				} else {
+					var AWSurl = s3.getSignedUrl('getObject', urlParams);
+					callback(null, AWSurl);
+				}
+			})
+		}
+	})
+}
+										
+function deleteAllUserFiles(userName, callback) {
+	returnAwsFilesByUser(userName, function(err, data) {
+		if (err) callback(err);
+		else {
+			var deleteKeyList = [];
+			data.forEach(function(file) {
+			 deleteKeyList.push({Key: file.Key});
+			});
+			var params = {
+				Bucket: bucketName, 
+				Delete: {Objects: deleteKeyList}
+			};
+			s3.deleteObjects(params, function(err, data) {
+				if(err) callback(err);
+				else callback(null, data);
+			})
+		}
+	})
+}
 
 
-//deleteBucket(params = {}, callback)
-//deleteObject(params = {}, callback) 
  
     
            
